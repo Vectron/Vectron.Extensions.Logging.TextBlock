a1c1c8eae2aeab5437777c396d16f49d9271087b
Vectron <thijs_bloebaum@hotmail.com>
2023-11-24 09:32:45 +0100

    Make the parser not slow down the ui

--- src/Vectron.Extensions.Logging.TextBlock/Internal/AnsiParsingTextblock.cs
+++ src/Vectron.Extensions.Logging.TextBlock/Internal/AnsiParsingTextblock.cs
@@ -9,7 +9,6 @@ namespace Vectron.Extensions.Logging.TextBlock.Internal;
 /// </summary>
 internal sealed class AnsiParsingTextBlock : ITextBlock
 {
-    private readonly AnsiParser parser;
     private readonly System.Windows.Controls.TextBlock textBlock;
     private int lines;
 
@@ -18,11 +17,11 @@ internal sealed class AnsiParsingTextBlock : ITextBlock
     /// </summary>
     /// <param name="textBlock">The textBlock we are writing to.</param>
     /// <param name="maxMessages">Maximum number of messages to display in the <see cref="TextBlock"/>.</param>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0290:Use primary constructor", Justification = "Make the property look strange.")]
     public AnsiParsingTextBlock(System.Windows.Controls.TextBlock textBlock, int maxMessages)
     {
         this.textBlock = textBlock;
         MaxMessages = maxMessages;
-        parser = new AnsiParser(WriteToTextBlock);
     }
 
     /// <inheritdoc/>
@@ -34,52 +33,71 @@ internal sealed class AnsiParsingTextBlock : ITextBlock
 
     /// <inheritdoc/>
     public void Write(string message)
-        => parser.Parse(message);
-
-    private void WriteToTextBlock(ReadOnlySpan<char> text, AnsiParserFoundStyle parsedStyle, IEnumerable<string> unknownCodes)
     {
-        var textString = text.ToString();
-        textBlock.Dispatcher.Invoke(() =>
-            {
-                if (textBlock.Inlines.Count > 0)
-                {
-                    lines = 0;
-                }
+        var parts = new Queue<MessagePart>();
+        var parser = new AnsiParser(Append);
+        parser.Parse(message);
+        textBlock.Dispatcher.Invoke(() => AddToTextBox(parts), System.Windows.Threading.DispatcherPriority.ApplicationIdle);
 
-                var (foregroundRed, foregroundGreen, foregroundBlue) = parsedStyle.ConvertForegroundColorToRGB();
-                var foregroundBrush = parsedStyle.HasForegroundColor
-                    ? new SolidColorBrush(Color.FromRgb((byte)foregroundRed, (byte)foregroundGreen, (byte)foregroundBlue))
-                    : Brushes.Black;
+        void Append(ReadOnlySpan<char> text, AnsiParserFoundStyle parsedStyle, IEnumerable<string> unknownCodes)
+        {
+            var textString = text.ToString();
+            var (foregroundRed, foregroundGreen, foregroundBlue) = parsedStyle.ConvertForegroundColorToRGB();
+            Color? foregroundColor = parsedStyle.HasForegroundColor
+                ? Color.FromRgb((byte)foregroundRed, (byte)foregroundGreen, (byte)foregroundBlue)
+                : null;
 
-                var (backgroundRed, backgroundGreen, backgroundBlue) = parsedStyle.ConvertBackgroundColorToRGB();
-                var backgroundBrush = parsedStyle.HasBackgroundColor
-                    ? new SolidColorBrush(Color.FromRgb((byte)backgroundRed, (byte)backgroundGreen, (byte)backgroundBlue))
-                    : null;
+            var (backgroundRed, backgroundGreen, backgroundBlue) = parsedStyle.ConvertBackgroundColorToRGB();
+            Color? backgroundBrush = parsedStyle.HasBackgroundColor
+                ? Color.FromRgb((byte)backgroundRed, (byte)backgroundGreen, (byte)backgroundBlue)
+                : null;
 
-                var run = new Run(textString)
-                {
-                    Background = backgroundBrush,
-                    Foreground = foregroundBrush,
-                };
-
-                textBlock.Inlines.Add(run);
-
-                if (textString.EndsWith(Environment.NewLine, StringComparison.Ordinal))
-                {
-                    lines++;
-                }
-
-                while (lines > MaxMessages
-                    && textBlock.Inlines.Count > 0)
-                {
-                    var firstItem = textBlock.Inlines.FirstInline;
-                    _ = textBlock.Inlines.Remove(textBlock.Inlines.FirstInline);
-                    if (firstItem is Run oldRun
-                        && oldRun.Text.EndsWith(Environment.NewLine, StringComparison.Ordinal))
-                    {
-                        lines--;
-                    }
-                }
-            });
+            var messagePart = new MessagePart(textString, foregroundColor, backgroundBrush);
+            parts.Enqueue(messagePart);
+        }
     }
+
+    private void AddToTextBox(Queue<MessagePart> messageParts)
+    {
+        if (textBlock.Inlines.Count == 0)
+        {
+            lines = 0;
+        }
+
+        lines++;
+        var newInline = new Queue<Run>(messageParts.Count);
+        while (messageParts.TryDequeue(out var messagePart))
+        {
+            var foregroundBrush = messagePart.ForeGround.HasValue
+                ? new SolidColorBrush(messagePart.ForeGround.Value)
+                : Brushes.Black;
+            var backgroundBrush = messagePart.BackGround.HasValue
+                ? new SolidColorBrush(messagePart.BackGround.Value)
+                : null;
+            var run = new Run(messagePart.Text)
+            {
+                Background = backgroundBrush,
+                Foreground = foregroundBrush,
+            };
+
+            newInline.Enqueue(run);
+        }
+
+        textBlock.Inlines.AddRange(newInline);
+        newInline.Clear();
+
+        while (lines > MaxMessages
+            && textBlock.Inlines.Count > 0)
+        {
+            var firstItem = textBlock.Inlines.FirstInline;
+            _ = textBlock.Inlines.Remove(textBlock.Inlines.FirstInline);
+            if (firstItem is Run oldRun
+                && oldRun.Text.EndsWith(Environment.NewLine, StringComparison.Ordinal))
+            {
+                lines--;
+            }
+        }
+    }
+
+    private record struct MessagePart(string Text, Color? ForeGround, Color? BackGround);
 }
